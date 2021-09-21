4f7d989671471fd74370097db1b2361645adbc89
abd elwahed dardeer <89838156+Eldardeery@users.noreply.github.com>
2021-09-21 12:01:24 +0200

    Update SevenSeg.h

--- Multi-Functional-Electric-Fan-master/FanCtrlProject/FanCtrlProject/Drivers/SevenSeg.h
+++ Multi-Functional-Electric-Fan-master/FanCtrlProject/FanCtrlProject/Drivers/SevenSeg.h
@@ -1,9 +1,4 @@
-/*
- * SevenSeg.h
- *
- * Created: 3/20/2020 5:40:37 AM
- *  Author: Mahmo
- */ 
+ 
 
 
 #ifndef SEVENSEG_H_
@@ -63,15 +58,15 @@ Often we need to use two, three or more SSDs and that too using only a single MC
 One possibility is that we use a bigger MCU with more I/O pins, like ATmega32, which has 26 I/O pins. But then we are still restricted to only a maximum of 3 SSDs that can be used.
 Another much better and recommended solution to this problem is to multiplex the Seven Segment Displays.
 "What is Multiplexing?"
-Wikipedia says ‘ In telecommunications and computer networks, multiplexing (also known as muxing) is a method by which multiple analog message signals or digital data streams are combined into one signal over a shared medium. The aim is to share an expensive resource.’
+Wikipedia says â€˜ In telecommunications and computer networks, multiplexing (also known as muxing) is a method by which multiple analog message signals or digital data streams are combined into one signal over a shared medium. The aim is to share an expensive resource.â€™
 What we mean by multiplexing of seven-segment display is that we will be using only 7 output ports to give the display on all of the SSDs.
 How to achieve this?
-Here, we will use ‘Persistence of Vision‘. Now you must have across this term already before. Yes, this is the same technique which is used in cinematography (display images so fast that our brain cannot distinguish any lag between two consecutive images). Similarly, when we mux more than one SSD, we display only one SSD at a time, and we switch between them so fast that our brain cannot distinguish between them.
-Lets say each display is active for only 5 milliseconds at a time, i.e. it gets lighted up 1/0.0045 times a seconds, that’s roughly equal to 222 times/second. Our eyes cannot sense a change so fast, and thus what we see is that all the displays are working simultaneously. What is actually happening in the hardware is that the MCU gives ‘1’ to the pin (remember, giving ‘1’ to the base of a BJT shorts the Collector and emitter junction?), which is connected to the base of the transistor of the respective displays, keeps the port ‘ON’ for 5 milliseconds, and then turns it off again. This procedure is put in an endless loop, so that we see the display continuously.
+Here, we will use â€˜Persistence of Visionâ€˜. Now you must have across this term already before. Yes, this is the same technique which is used in cinematography (display images so fast that our brain cannot distinguish any lag between two consecutive images). Similarly, when we mux more than one SSD, we display only one SSD at a time, and we switch between them so fast that our brain cannot distinguish between them.
+Lets say each display is active for only 5 milliseconds at a time, i.e. it gets lighted up 1/0.0045 times a seconds, thatâ€™s roughly equal to 222 times/second. Our eyes cannot sense a change so fast, and thus what we see is that all the displays are working simultaneously. What is actually happening in the hardware is that the MCU gives â€˜1â€™ to the pin (remember, giving â€˜1â€™ to the base of a BJT shorts the Collector and emitter junction?), which is connected to the base of the transistor of the respective displays, keeps the port â€˜ONâ€™ for 5 milliseconds, and then turns it off again. This procedure is put in an endless loop, so that we see the display continuously.
 So now we are clear with what actually is multiplexing and how it works! But how to achieve this? There are two methods:
 Using timers and delay loops
 Using ( _delay_ ) commands
-We will here use the 2nd option because it is simpler to understand. If you wish to use to use the first option, then I would suggest you to read Mayank’s awesome timer tutorials.
+We will here use the 2nd option because it is simpler to understand. If you wish to use to use the first option, then I would suggest you to read Mayankâ€™s awesome timer tutorials.
 #endif //0
 
 
